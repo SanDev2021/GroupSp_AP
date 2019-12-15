c410ee4ef8545072d243f302b323324a88086bc4
sandeepLove <57155806+sandeepLove@users.noreply.github.com>
2019-12-15 17:17:49 -0500

    app data class

--- app/src/main/java/com/example/projectandroid/AppData.java
+++ app/src/main/java/com/example/projectandroid/AppData.java
@@ -5,6 +5,6 @@ import com.example.projectandroid.employee.Employee;
 import java.util.ArrayList;
 
 public class AppData {
-    public static ArrayList<Employee> employees = new ArrayList<>();
+
 
 }
