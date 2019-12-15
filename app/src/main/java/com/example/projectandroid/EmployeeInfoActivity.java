8ddf9d3ee12270093ab6a66324e47b23bdc5825a
Sandeep <sandeep.mahal6@gmail.com>
2019-12-15 16:38:14 -0500

    employee information textview set

--- app/src/main/java/com/example/projectandroid/EmployeeInfoActivity.java
+++ app/src/main/java/com/example/projectandroid/EmployeeInfoActivity.java
@@ -11,10 +11,10 @@ public class EmployeeInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_info);
+TextView tvInfo=findViewById(R.id.tvInfo);
+String info=getIntent().getStringExtra("INFO");
+tvInfo.setText(info);
 
-        TextView tvInfo = findViewById(R.id.tvInfo);
-        String info = getIntent().getStringExtra("INFO");
-        tvInfo.setText(info);
     }
 
 }
